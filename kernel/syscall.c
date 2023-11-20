bd390b855c1a4db0f4787bad37cdec1be7a513f3
KarylDAZE <980596745@qq.com>
2023-11-20 22:59:04 +0800

    add print backtrace to syscall path

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -16,7 +16,8 @@
 //
 // implement the SYS_user_print syscall
 //
-ssize_t sys_user_print(const char* buf, size_t n) {
+ssize_t sys_user_print(const char *buf, size_t n)
+{
   sprint(buf);
   return 0;
 }
@@ -24,24 +25,34 @@ ssize_t sys_user_print(const char* buf, size_t n) {
 //
 // implement the SYS_user_exit syscall
 //
-ssize_t sys_user_exit(uint64 code) {
+ssize_t sys_user_exit(uint64 code)
+{
   sprint("User exit with code:%d.\n", code);
-  // in lab1, PKE considers only one app (one process). 
+  // in lab1, PKE considers only one app (one process).
   // therefore, shutdown the system when the app calls exit()
   shutdown(code);
+}
+
+//
+// implement the SYS_user_print_backtrace syscall
+//
+ssize_t sys_user_print_backtrace(int backStep)
+{
 }
 
 //
 // [a0]: the syscall number; [a1] ... [a7]: arguments to the syscalls.
 // returns the code of success, (e.g., 0 means success, fail for otherwise)
 //
-long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, long a7) {
-  switch (a0) {
-    case SYS_user_print:
-      return sys_user_print((const char*)a1, a2);
-    case SYS_user_exit:
-      return sys_user_exit(a1);
-    default:
-      panic("Unknown syscall %ld \n", a0);
+long do_syscall(long a0, long a1, long a2, long a3, long a4, long a5, long a6, long a7)
+{
+  switch (a0)
+  {
+  case SYS_user_print:
+    return sys_user_print((const char *)a1, a2);
+  case SYS_user_exit:
+    return sys_user_exit(a1);
+  default:
+    panic("Unknown syscall %ld \n", a0);
   }
 }
