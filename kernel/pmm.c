5541d9b46392a69f53b17ea64394712ab2a9f928
KarylDAZE <980596745@qq.com>
2024-03-07 13:27:24 +0800

    add memory lock & unlock

--- kernel/pmm.c
+++ kernel/pmm.c
@@ -9,14 +9,15 @@
 // _end is defined in kernel/kernel.lds, it marks the ending (virtual) address of PKE kernel
 extern char _end[];
 // g_mem_size is defined in spike_interface/spike_memory.c, it indicates the size of our
-// (emulated) spike machine. g_mem_size's value is obtained when initializing HTIF. 
+// (emulated) spike machine. g_mem_size's value is obtained when initializing HTIF.
 extern uint64 g_mem_size;
 
-static uint64 free_mem_start_addr;  //beginning address of free memory
-static uint64 free_mem_end_addr;    //end address of free memory (not included)
+static uint64 free_mem_start_addr; // beginning address of free memory
+static uint64 free_mem_end_addr;   // end address of free memory (not included)
 
-int vm_alloc_stage[NCPU] = { 0 }; // 0 for kernel alloc, 1 for user alloc
-typedef struct node {
+int vm_alloc_stage[NCPU] = {0}; // 0 for kernel alloc, 1 for user alloc
+typedef struct node
+{
   struct node *next;
 } list_node;
 
@@ -27,16 +28,21 @@ static list_node g_free_mem_list;
 // actually creates the freepage list. each page occupies 4KB (PGSIZE), i.e., small page.
 // PGSIZE is defined in kernel/riscv.h, ROUNDUP is defined in util/functions.h.
 //
-static void create_freepage_list(uint64 start, uint64 end) {
+static void create_freepage_list(uint64 start, uint64 end)
+{
   g_free_mem_list.next = 0;
   for (uint64 p = ROUNDUP(start, PGSIZE); p + PGSIZE < end; p += PGSIZE)
-    free_page( (void *)p );
+    free_page((void *)p);
 }
+
+int mem_lock = 0;
 
 //
 // place a physical page at *pa to the free list of g_free_mem_list (to reclaim the page)
 //
-void free_page(void *pa) {
+void free_page(void *pa)
+{
+  spin_lock(&mem_lock);
   if (((uint64)pa % PGSIZE) != 0 || (uint64)pa < free_mem_start_addr || (uint64)pa >= free_mem_end_addr)
     panic("free_page 0x%lx \n", pa);
 
@@ -44,47 +50,72 @@ void free_page(void *pa) {
   list_node *n = (list_node *)pa;
   n->next = g_free_mem_list.next;
   g_free_mem_list.next = n;
+  spin_unlock(&mem_lock);
 }
 
 //
 // takes the first free page from g_free_mem_list, and returns (allocates) it.
 // Allocates only ONE page!
 //
-void *alloc_page(void) {
+void *alloc_page(void)
+{
   list_node *n = g_free_mem_list.next;
   uint64 hartid = 0;
-  if (vm_alloc_stage[hartid]) {
+  spin_lock(&mem_lock);
+  if (vm_alloc_stage[hartid])
+  {
     sprint("hartid = %ld: alloc page 0x%x\n", hartid, n);
   }
-  if (n) g_free_mem_list.next = n->next;
+  if (n)
+    g_free_mem_list.next = n->next;
+  spin_unlock(&mem_lock);
   return (void *)n;
+}
+
+void spin_lock(int *lock)
+{
+  int lock_flag = 1;
+  // use amoswap to implement spin lock
+  while (lock_flag)
+  {
+    asm volatile("amoswap.w %0,%0,(%1)\n"
+                 : "+r"(lock_flag), "+r"(lock)
+                 :
+                 : "memory");
+  }
+}
+
+void spin_unlock(int *lock)
+{
+  *lock = 0;
 }
 
 //
 // pmm_init() establishes the list of free physical pages according to available
 // physical memory space.
 //
-void pmm_init() {
+void pmm_init()
+{
   // start of kernel program segment
   uint64 g_kernel_start = KERN_BASE;
   uint64 g_kernel_end = (uint64)&_end;
 
   uint64 pke_kernel_size = g_kernel_end - g_kernel_start;
   sprint("PKE kernel start 0x%lx, PKE kernel end: 0x%lx, PKE kernel size: 0x%lx .\n",
-    g_kernel_start, g_kernel_end, pke_kernel_size);
+         g_kernel_start, g_kernel_end, pke_kernel_size);
 
   // free memory starts from the end of PKE kernel and must be page-aligined
-  free_mem_start_addr = ROUNDUP(g_kernel_end , PGSIZE);
+  free_mem_start_addr = ROUNDUP(g_kernel_end, PGSIZE);
 
   // recompute g_mem_size to limit the physical memory space that our riscv-pke kernel
   // needs to manage
   g_mem_size = MIN(PKE_MAX_ALLOWABLE_RAM, g_mem_size);
-  if( g_mem_size < pke_kernel_size )
-    panic( "Error when recomputing physical memory size (g_mem_size).\n" );
+  if (g_mem_size < pke_kernel_size)
+    panic("Error when recomputing physical memory size (g_mem_size).\n");
 
   free_mem_end_addr = g_mem_size + DRAM_BASE;
   sprint("free physical memory address: [0x%lx, 0x%lx] \n", free_mem_start_addr,
-    free_mem_end_addr - 1);
+         free_mem_end_addr - 1);
 
   sprint("kernel memory manager is initializing ...\n");
   // create the list of free pages
--- kernel/pmm.h
+++ kernel/pmm.h
@@ -6,9 +6,13 @@
 // Initialize phisical memeory manager
 void pmm_init();
 // Allocate a free phisical page
-void* alloc_page();
+void *alloc_page();
 // Free an allocated page
-void free_page(void* pa);
+void free_page(void *pa);
+// lock when alloc or free page
+void spin_lock(int *lock);
+// unlock when alloc or free end
+void spin_unlock(int *lock);
 
 extern int vm_alloc_stage[NCPU];
 
