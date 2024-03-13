a5be6522f3f8e44cc9b4de521c1aaa537caeaaee
KarylDAZE <980596745@qq.com>
2024-03-13 08:49:44 +0800

    add map but not copy child heap

--- kernel/process.c
+++ kernel/process.c
@@ -216,17 +216,18 @@ int do_fork(process *parent)
           free_block_filter[index] = 1;
         }
 
-        // copy and map the heap blocks
+        // only map the heap blocks (copy-on-write)
         for (uint64 heap_block = current->user_heap.heap_bottom;
              heap_block < current->user_heap.heap_top; heap_block += PGSIZE)
         {
           if (free_block_filter[(heap_block - heap_bottom) / PGSIZE]) // skip free blocks
             continue;
 
-          void *child_pa = alloc_page();
-          memcpy(child_pa, (void *)lookup_pa(parent->pagetable, heap_block), PGSIZE);
-          user_vm_map((pagetable_t)child->pagetable, heap_block, PGSIZE, (uint64)child_pa,
-                      prot_to_type(PROT_WRITE | PROT_READ, 1));
+          // void *child_pa = alloc_page();
+          // memcpy(child_pa, (void *)lookup_pa(parent->pagetable, heap_block), PGSIZE);
+          void *parent_pa = (void *)lookup_pa(parent->pagetable, heap_block);
+          user_vm_map((pagetable_t)child->pagetable, heap_block, PGSIZE, (uint64)parent_pa,
+                      prot_to_type(PROT_READ, 1));
         }
 
         child->mapped_info[HEAP_SEGMENT].npages = parent->mapped_info[HEAP_SEGMENT].npages;
